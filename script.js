6d98cbfce317834356bdbbabed88460a87b67d69
everinurmind <nurbol.sultanov@gmail.com>
2023-02-07 14:22:48 -0600

    edited mobile menu

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
-const navigationMenu = document.querySelector('.navigation-menu');
+const navigationMenu = document.querySelector('.navbar');
+const logo = document.querySelector('.logo');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('change');
   navigationMenu.classList.toggle('show');
   navigationMenu.classList.toggle('change');
+  logo.classList.toggle('change');
 });
