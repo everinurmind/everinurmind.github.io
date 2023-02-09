7db8868f0731e2b828b1a7ada060ea8ddb4ac08f
everinurmind <nurbol.sultanov@gmail.com>
2023-02-09 00:38:33 -0600

    Created the works section dynamically (start)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Navigation Menu JS
+
 const hamburger = document.querySelector('.hamburger');
 const navigationMenu = document.querySelector('.navbar');
 const logo = document.querySelector('.logo');
@@ -15,6 +17,12 @@ document.querySelectorAll('.navigation').forEach((link) => link.addEventListener
   navigationMenu.classList.remove('change');
   logo.classList.remove('change');
 }));
+
+// Works Section JS
+
+const worksContainer = document.querySelector('.works-container');
+const containerProject = document.querySelector('.works');
+const overlay = document.querySelector('.overlay');
 
 const cardsContainer = [
 
@@ -90,3 +98,94 @@ const cardsContainer = [
     source: '#',
   },
 ];
+
+const displayModal = (id) => {
+  const displayIdOf = cardsContainer.findIndex((card) => card.id === +id);
+  worksContainer.style.display = 'flex';
+  overlay.style.display = 'block';
+  let html = '';
+
+  html += `<div class= "title">
+      <h2 class="heading">${cardsContainer[displayIdOf].name}</h2>
+      <button class="close">
+        <i class="las la-times"></i>
+      </button>
+      </div>
+      <ul class="frame">
+          <li class="client">${cardsContainer[displayIdOf].titleDescription[0]}</li>
+          <li class="dot">&#x2022;</li>
+          <li class="role">${cardsContainer[displayIdOf].titleDescription[1]}</li>
+          <li class="dot">&#x2022;</li>
+          <li class="year">${cardsContainer[displayIdOf].titleDescription[2]}</li>
+      </ul>
+      <img class="project-image" src="${cardsContainer[displayIdOf].image}">
+      <div class='top'>
+        <div class=desc>
+          <p class="project-description">${cardsContainer[displayIdOf].description}</p>
+        </div>
+        <div class='bottom'>
+          <ul class="tags">
+            <li class="language">${cardsContainer[displayIdOf].technologies[0]}</li>
+            <li class="language">${cardsContainer[displayIdOf].technologies[1]}</li>
+            <li class="language">${cardsContainer[displayIdOf].technologies[2]}</li>
+          </ul>
+  
+          <div class="action">
+            <button class="action">
+              <a class="btn-seeLive" href="${cardsContainer[displayIdOf].liveVersion}"> See live </a>
+              <i class="las la-external-link-alt" id="share-sign"></i>
+            </button>
+            <button class="action">
+              <a class="btn-seeSource" href="${cardsContainer[displayIdOf].source}">See source </a>
+              <i class="lab la-github" id="git-sign"></i>
+            </button>
+          </div>
+        </div>
+      </div>`;
+
+  worksContainer.innerHTML = html;
+
+  const BtnClose = document.querySelector('.close');
+
+  BtnClose.addEventListener('click', (e) => {
+    e.preventDefault();
+    worksContainer.style.display = 'none';
+    overlay.style.display = 'none';
+  });
+};
+
+cardsContainer.forEach((card) => {
+  const boxCard = document.createElement('div');
+  boxCard.className = 'box-card';
+  const html = `
+  <div class="works-card">
+  <img class="project-image" src="${card.image}" alt=${card.name}>
+                <div class="info-block">
+                    <div class="top">
+                        <h2 id="six">${card.name}</h2>
+                        <ul class="frame">
+                            <li id="client">${card.titleDescription[0]}</li>
+                            <li id="dot"> </li>
+                            <li id="role">${card.titleDescription[1]}</li>
+                            <li id="dot"> </li>
+                            <li id="year">${card.titleDescription[2]}</li></ul>
+                    </div>
+                            <p>${card.description}</p>
+                    <div class="bottom">
+                        <ul class="tags">
+                        <li>${card.technologies[0]}</li>
+                        <li>${card.technologies[1]}</li>
+                        <li>${card.technologies[2]}</li></ul>
+                        <button class="action" id="${card.id}">See Project</button>
+                    </div>
+                </div>
+            </div>
+  `;
+  containerProject.insertAdjacentHTML('afterbegin', html);
+});
+
+const actbtn = document.querySelectorAll('.action');
+actbtn.forEach((action) => action.addEventListener('click', (e) => {
+  const { id } = e.target;
+  displayModal(id);
+}));
